4f9e75d39c5796bc88096d2e0b5e805291bdb711
InGwonKang <tnqkr3494@naver.com>
2024-03-14 17:46:17 +0900

    tailwind_odd&even

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex justify-center items-center p-8 capitalize">
-      <div className="bg-white w-full rounded-lg p-6 max-w-screen-sm shadow-lg flex flex-col has-[:invalid]:bg-red-300">
-        <input
-          type="email"
-          placeholder="Email"
-          required
-          className="bg-gray-300 rounded-full px-4 py-2 mb-4 outline-none focus:ring ring-green-500 ring-offset-2 invalid:focus:ring-red-500 peer"
-        />
-        <span className="text-red hidden peer-invalid:block">
-          Email is required
-        </span>
-        <button className="bg-black text-white px-6 py-2 rounded-full active:scale-90 transition-transform">
-          Log in
-        </button>
+      <div className="bg-white w-full rounded-sm p-6 max-w-screen-sm shadow-lg flex flex-col has-[:invalid]:bg-red-300 gap-2">
+        {["Nico", "Me", "You", "Nickelback"].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 odd:bg-cyan-300 rounded-xl p-3"
+          >
+            <div className="size-7 bg-cyan-500 rounded-full" />
+            <span>{person}</span>
+            <div className="size-6 bg-red-500 flex items-center justify-center rounded-full text-white text-sm">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
