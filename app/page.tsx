9bdf684180ebefe9ff2769a4d2b6b877dbe54586
InGwonKang <tnqkr3494@naver.com>
2024-03-14 14:36:50 +0900

    tailwind_peer

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,19 @@
 export default function Home() {
   return (
-    <main className="bg-gray-400 h-screen flex justify-center items-center p-8 capitalize">
-      <div className="bg-white w-full rounded-md p-6">
-        <div className="flex justify-between">
-          <div className="flex flex-col">
-            <span className="text-gray-600">in transit</span>
-            <span className="text-2xl font-bold -mt-2">coolblue</span>
-          </div>
-          <div className="w-10 h-10 bg-orange-400 rounded-full" />
-        </div>
-        <div className="flex items-center gap-2 mb-2">
-          <span className="text-white bg-green-400 px-2.5 py-1 rounded-xl hover:bg-green-600 transition hover:scale-110">
-            Today
-          </span>
-          <span>9:30-10:30u</span>
-        </div>
-        <div className="relative">
-          <div className="w-full bg-gray-300 h-2 rounded-lg absolute" />
-          <div className="w-2/3 bg-green-300 h-2 rounded-lg absolute" />
-        </div>
-        <div className="mt-4 flex justify-between text-gray-500">
-          <span>expected</span>
-          <span>sorting center</span>
-          <span>in transit</span>
-          <span className="text-gray-400">Deliverd</span>
-        </div>
+    <main className="bg-gray-100 h-screen flex justify-center items-center p-8 capitalize">
+      <div className="bg-white w-full rounded-md p-6 max-w-screen-sm shadow-lg flex flex-col">
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          className="bg-gray-300 rounded-full px-4 py-2 mb-4 outline-none focus:ring ring-green-500 ring-offset-2 invalid:focus:ring-red-500 peer"
+        />
+        <span className="text-red hidden peer-invalid:block">
+          Email is required
+        </span>
+        <button className="bg-black text-white px-6 py-2 rounded-full active:scale-90 transition-transform">
+          Log in
+        </button>
       </div>
     </main>
   );
