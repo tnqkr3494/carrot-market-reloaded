4ff1220dd2f79aab391fef8b700337e38f6c94ef
InGwonKang <tnqkr3494@naver.com>
2024-03-30 12:27:29 +0900

    login_interactive

--- app/sms/action.ts
+++ app/sms/action.ts
@@ -2,12 +2,53 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Invalid Phone Format"
+  );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState: any, formData: FormData) {
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
+interface ActionState {
+  token: boolean;
+}
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+
+  // 번호를 입력 할 때(token입력 부분은 가려짐)
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    // 번호를 잘못 입력했을 때
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    }
+    // 번호를 잘 입력하면 토큰화면 보여주기
+    else {
+      return {
+        token: true,
+      };
+    }
+  }
+  // 번호 입력 후 토큰 입력
+  else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,13 @@ import FormInput from "@/components/form-input";
 import { useFormState } from "react-dom";
 import { smsLogin } from "./action";
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function Home() {
-  const [state, action] = useFormState(smsLogin, null);
+  const [state, action] = useFormState(smsLogin, initialState);
   return (
     <div className="py-8 px-6 gap-10 flex flex-col">
       <div className="font-medium">
@@ -14,18 +19,23 @@ export default function Home() {
         <h2 className="mt-3">Verify your phone number</h2>
       </div>
       <form action={action} className="flex flex-col gap-3">
-        <FormInput
-          name="phone"
-          type="number"
-          required
-          placeholder="Phone number"
-        />
-        <FormInput
-          name="token"
-          type="number"
-          required
-          placeholder="Verification code"
-        />
+        {state.token ? (
+          <FormInput
+            name="token"
+            type="number"
+            required
+            placeholder="Verification code"
+            errors={state.error?.formErrors}
+          />
+        ) : (
+          <FormInput
+            name="phone"
+            type="text"
+            required
+            placeholder="Phone number"
+            errors={state.error?.formErrors}
+          />
+        )}
         <FormButton loading={false} text="Verify" />
       </form>
     </div>
