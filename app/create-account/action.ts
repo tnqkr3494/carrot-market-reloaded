327cb74da86a43d5b77c41f86d8de470a02f8d46
InGwonKang <tnqkr3494@naver.com>
2024-03-31 11:38:32 +0900

    database_validation

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -4,6 +4,7 @@ import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
 } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
 
 const checkUsername = (username: string) => {
@@ -20,6 +21,30 @@ const checkPassword = ({
   return password === confirm;
 };
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -30,13 +55,16 @@ const formSchema = z
       .min(5, "Too Short")
       .max(10, "Too Long")
       .toLowerCase()
-      .transform((username) => `${username}❤️`)
-      .refine(checkUsername, "no kang"),
-    email: z.string().email().toLowerCase(),
-    password: z
+      //.transform((username) => `${username}❤️`)
+      .refine(checkUsername, "no kang")
+      .refine(checkUniqueUsername, "This username is already taken"),
+    email: z
       .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(PASSWORD_REGEX, PASSWORD_ERROR),
+      .email()
+      .toLowerCase()
+      .refine(checkUniqueEmail, "This email is already taken"),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    //.regex(PASSWORD_REGEX, PASSWORD_ERROR),
     confirm: z.string().min(PASSWORD_MIN_LENGTH),
   })
   .refine(checkPassword, {
@@ -51,7 +79,7 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirm: formData.get("confirm"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   }
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const PASSWORD_MIN_LENGTH = 10;
+export const PASSWORD_MIN_LENGTH = 4;
 
 export const PASSWORD_REGEX = new RegExp(
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
