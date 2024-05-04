534328dd8059b5b22e30e22e972b7d614773ed8c
InGwonKang <tnqkr3494@naver.com>
2024-05-04 21:22:04 +0900

    github_oauth_3

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -1,4 +1,6 @@
-import { notFound } from "next/navigation";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -12,7 +14,7 @@ export async function GET(req: NextRequest) {
     code,
   }).toString();
   const accessTokenURL = `https://github.com/login/oauth/access_token?${params}`;
-  const tokenResponse = await (
+  const { error, access_token } = await (
     await fetch(accessTokenURL, {
       method: "POST",
       headers: {
@@ -20,10 +22,46 @@ export async function GET(req: NextRequest) {
       },
     })
   ).json();
-  if ("error" in tokenResponse) {
+  if (error) {
+    // new를 사용해서 Response 설정하기
     return new Response(null, {
       status: 400,
     });
   }
-  return Response.json({ tokenResponse });
+  const { login, id, avatar_url } = await (
+    await fetch("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+      cache: "no-cache",
+    })
+  ).json();
+
+  const user = await db.user.findUnique({
+    where: {
+      github_id: String(id),
+    },
+    select: {
+      id: true,
+    },
+  });
+  // user가 기존에 존재하는 user면 바로 로그인 직행
+  if (user) {
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
+    return redirect("/profile");
+  }
+
+  const newUser = await db.user.create({
+    data: {
+      github_id: String(id),
+      username: login,
+      avatar: avatar_url,
+    },
+  });
+  const session = await getSession();
+  session.id = newUser.id;
+  await session.save();
+  return redirect("/profile");
 }
