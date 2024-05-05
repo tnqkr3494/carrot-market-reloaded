a978eeb67a89ecc525b3661c5906761cf0aab4a2
InGwonKang <tnqkr3494@naver.com>
2024-05-05 15:00:49 +0900

    product_component

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,28 @@
-export default function Product() {
+import ListProducts from "@/components/list-product";
+import db from "@/lib/db";
+
+async function getProducts() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+    },
+  });
+  return products;
+}
+
+export default async function Product() {
+  const products = await getProducts();
   return (
     <div>
-      <h1 className="text-white text-4xl">Product!</h1>
+      <div className="p-5 flex flex-col gap-5">
+        {products.map((product) => (
+          <ListProducts key={product.id} {...product} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ components/list-product.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ListProductsProps {
+  id: number;
+  title: string;
+  price: number;
+  created_at: Date;
+  photo?: string | null;
+}
+
+export default function ListProducts({
+  id,
+  title,
+  price,
+  created_at,
+  photo,
+}: ListProductsProps) {
+  return (
+    <Link href={`/products/${id}`} className="flex gap-5">
+      <div className="relative size-28 rounded-md overflow-hidden">
+        {!photo ? (
+          <div className="absolute bg-slate-500 size-28 flex items-center justify-center">
+            <span className="text-white">No img</span>
+          </div>
+        ) : (
+          <Image fill src={photo!} alt={title} quality={100} />
+        )}
+      </div>
+      <div className="flex flex-col gap-1 *:text-white">
+        <span className="text-lg">{title}</span>
+        <span className="text-sm text-neutral-500">
+          {created_at.toString()}
+        </span>
+        <span className="text-lg font-semibold">{price}</span>
+      </div>
+    </Link>
+  );
+}
