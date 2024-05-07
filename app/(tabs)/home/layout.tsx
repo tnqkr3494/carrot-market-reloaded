5548ec65fffd19cb8f8281ed4bc9cb6e63df9f6b
InGwonKang <tnqkr3494@naver.com>
2024-05-07 18:31:54 +0900

    parallel_modal

---
+++ app/(tabs)/home/layout.tsx
@@ -0,0 +1,16 @@
+export default function HomeLayout({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
+  return (
+    <div>
+      <>
+        {children}
+        {modal}
+      </>
+    </div>
+  );
+}
