11bbba263ec6a43006e58123662433372fa68414
InGwonKang <tnqkr3494@naver.com>
2024-05-14 21:34:34 +0900

    nextJS14_log

--- app/_extras/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Extras() {
-  return (
-    <div className="flex flex-col gap-3 py-10">
-      <h1 className="text-6xl font-rubik">Extras!</h1>
-      <h2 className="font-roboto">So much more to learn!</h2>
-    </div>
-  );
-}
