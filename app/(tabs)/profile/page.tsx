0b9fc88fa1a1d6a3f9370a40b44b9987df96a25b
InGwonKang <tnqkr3494@naver.com>
2024-05-16 21:35:51 +0900

    profile_page

--- app/(tabs)/profile/page.tsx
+++ app/(tabs)/profile/page.tsx
@@ -1,5 +1,11 @@
 import db from "@/lib/db";
 import getSession from "@/lib/session";
+import {
+  CurrencyDollarIcon,
+  HeartIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/solid";
+import Image from "next/image";
 import { notFound, redirect } from "next/navigation";
 
 async function gertUser() {
@@ -26,12 +32,45 @@ export default async function Profile() {
     redirect("/");
   };
   return (
-    <div>
-      <h1>Welcome! {user?.username}</h1>
+    <div className="flex flex-col gap-5 p-5">
+      <div>
+        <form action={logOut} className="flex justify-end">
+          <button className="flex bg-red-500 p-2 rounded-full font-semibold hover:bg-red-600 transition-colors">
+            Log out
+          </button>
+        </form>
+      </div>
       {/* onclick사용안하고 trick으로 사용하는 방법 */}
-      <form action={logOut}>
-        <button>Log out</button>
-      </form>
+      <div className="flex items-center gap-5 border-y-2 p-4">
+        {user.avatar ? (
+          <Image
+            className="size-40 rounded-full"
+            width={160}
+            height={160}
+            src={user.avatar}
+            alt={user.username}
+          />
+        ) : null}
+        <div>
+          <span className="font-semibold text-2xl">
+            {user.username}님 환영합니다!
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-around gap-4 *:flex *:flex-col *:items-center *:cursor-pointer *:text-xl *:font-semibold">
+        <div className="bg-orange-500 p-8 rounded-md hover:bg-orange-600 transition hover:scale-110">
+          <CurrencyDollarIcon className="size-20" />
+          <span>Sell</span>
+        </div>
+        <div className="bg-orange-500 p-8 rounded-md hover:bg-orange-600 transition hover:scale-110">
+          <ShoppingCartIcon className="size-20" />
+          <span>Buy</span>
+        </div>
+        <div className="bg-orange-500 p-8 rounded-md hover:bg-orange-600 transition hover:scale-110">
+          <HeartIcon className="size-20" />
+          <span>Like</span>
+        </div>
+      </div>
     </div>
   );
 }
